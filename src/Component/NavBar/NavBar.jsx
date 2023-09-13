dcbc37fb9cd0aafa71dd50394e284ba76139e729
minaabskhron <minaabskhron@gmail.com>
2023-09-13 20:04:20 +0300

    fourth

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery'
 
 import './NavBar.css'
 import { Link } from 'react-router-dom';
@@ -9,24 +10,24 @@ export default class NavBar extends Component {
         <>
         <nav className="navbar navbar-expand-lg myNavBar py-4">
         <div className="container">
-            <Link className="navbar-brand " to="/">START FRAMEWORK</Link>
+            <Link className="navbar-brand myNavBarLinks" to="/">START FRAMEWORK</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 
-                <li className="nav-item">
-                <Link className="nav-link " to="about">ABOUT</Link>
+                <li className="nav-item pe-2">
+                <Link className="nav-link myNavBarLinks" to="about">ABOUT</Link>
                 </li>
 
 
-                <li className="nav-item">
-                <Link className="nav-link" to="portfolio">PORTFOLIO</Link>
+                <li className="nav-item pe-2">
+                <Link className="nav-link myNavBarLinks" to="portfolio">PORTFOLIO</Link>
                 </li>
 
-                <li className="nav-item">
-                <Link className="nav-link" to="contact">CONTACT</Link>
+                <li className="nav-item pe-2">
+                <Link className="nav-link myNavBarLinks" to="contact">CONTACT</Link>
                 </li>
                 </ul>
             </div>
@@ -34,5 +35,17 @@ export default class NavBar extends Component {
         </nav>
         </>
     );
+  }
+
+  componentDidMount(){
+    const navLinks = $(".myNavBarLinks")
+    navLinks.click(
+      function(e)
+      {
+        navLinks.removeClass("navbarColor")
+        $(e.target).addClass("navbarColor").addClass("rounded-3")
+        $(".navbar-brand").removeClass("navbarColor")
+      }
+    )
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'jquery/dist/jquery.min.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
